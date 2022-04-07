5c6f88e70ea8d1138cec63c138cba4dfbc1b98af
Dimarik <krasnodar93@mail.ru>
2022-04-07 10:51:20 +0300

    07-04 - екшфтпду

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -16,4 +16,14 @@ public class TriangleTest {
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
     }
+
+    @Test
+    public void whenUnreal() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
+        Point c = new Point(2, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
+    }
 }
