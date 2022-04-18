abc0520f6a6e909598aee89528805448138ce43b
Dimarik <krasnodar93@mail.ru>
2022-04-18 10:39:14 +0300

    1804 - replace

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -43,7 +43,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            item.setId(index);
+            item.setId(id);
             items[index] = item;
         }
         return rsl;
