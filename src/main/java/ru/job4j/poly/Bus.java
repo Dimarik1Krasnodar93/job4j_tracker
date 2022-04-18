52fd9620576d5de207f4283aa11b62290737add9
Dimarik <krasnodar93@mail.ru>
2022-04-18 14:06:11 +0300

    1804 - interface Bus

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void getPassangeres() {
+
+    }
+
+    @Override
+    public void refuel(double count, double prioe) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    public void move();
+
+    public void getPassangeres();
+
+    public void refuel(double count, double prioe);
+}
