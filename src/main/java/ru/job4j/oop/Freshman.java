424085982ca992fb993cbfa30ca83f387098c71e
Dimarik <krasnodar93@mail.ru>
2022-04-20 18:00:41 +0300

    2004 - Приведение типов

--- src/main/java/ru/job4j/oop/Freshman.java
+++ src/main/java/ru/job4j/oop/Freshman.java
@@ -2,4 +2,9 @@ package ru.job4j.oop;
 
 public class Freshman extends Student {
 
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = (Object) freshman;
+    }
 }
