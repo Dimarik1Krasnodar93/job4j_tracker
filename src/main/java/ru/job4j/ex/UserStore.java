b98796d87eae18998dc429865bfcf0e612104b9f
Dimarik <krasnodar93@mail.ru>
2022-05-02 10:35:36 +0300

    02-05 - 0.5. Иерархия исключений и множественный catch. [#219368 #295913] -1035

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,6 +6,7 @@ public class UserStore {
         for (User user : users) {
             if (user.getUsername().equals(login)) {
                 result = user;
+                break;
             }
         }
         if (result == null) {
@@ -15,17 +16,10 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        boolean result = false;
-        try {
-            user.isValid();
-            result = true;
-        } catch (UserInvalidException ex) {
-            ex.printStackTrace();
-        }
         if (!user.isValid()) {
             throw new UserInvalidException("Пользователь не валидный");
         }
-        return result;
+        return true;
     }
 
     public static void main(String[] args) {
