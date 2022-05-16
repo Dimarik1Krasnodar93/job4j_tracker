833b07bb564daab53e1e3f1eb1c6a03ba08f9f6d
Dimarik <krasnodar93@mail.ru>
2022-05-16 16:09:54 +0300

    16-05 - 3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,56 @@
+package ru.job4j.bank;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        users.get(findByPassport(passport)).add(account);
+    }
+
+    public User findByPassport(String passport) {
+        User rsl = null;
+        for (Map.Entry<User, List<Account>> user : users.entrySet()) {
+            if (user.getKey().getPassport().equals(passport)) {
+                rsl = user.getKey();
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        Account rsl = null;
+        if (user != null) {
+            for (Account account : users.get(user)) {
+                if (account.getRequisite().equals(requisite)) {
+                    rsl = account;
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Account destAcc = findByRequisite(destPassport, destRequisite);
+        boolean rsl = false;
+        if (srcAcc != null && destAcc != null && srcAcc.getBalance() >= amount) {
+            srcAcc.setBalance(srcAcc.getBalance() - amount);
+            destAcc.setBalance(destAcc.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
