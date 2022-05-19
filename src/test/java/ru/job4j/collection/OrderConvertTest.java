06bb9fb510dbbee6f0d27b26d10d421d66bb793b
Dimarik <krasnodar93@mail.ru>
2022-05-19 09:10:00 +0300

    1905-0905

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -16,7 +16,7 @@ public class OrderConvertTest {
         List<Order> orders = new ArrayList<>();
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
-        assertThat(map.size(), is(1));
+        assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
 
     @Test
