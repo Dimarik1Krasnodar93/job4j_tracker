b096af920f95c961f1daf53b2a84c804e24d6a07
Dimarik <krasnodar93@mail.ru>
2022-05-23 09:11:14 +0300

    23-05- 09 11

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -8,10 +8,6 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] strArrayLeft = left.split("\\.");
         String[] strArrayRight = right.split("\\.");
-        if (strArrayLeft.length == 0 || strArrayRight.length == 0) {
-            return 0;
-        } else {
-            return Integer.compare(Integer.parseInt(strArrayLeft[0]), Integer.parseInt(strArrayRight[0]));
-        }
+        return Integer.compare(Integer.parseInt(strArrayLeft[0]), Integer.parseInt(strArrayRight[0]));
     }
 }
