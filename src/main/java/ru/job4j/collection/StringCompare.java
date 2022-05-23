577729ba36d0e9c0483735392963a2c9a04a3332
Dimarik <krasnodar93@mail.ru>
2022-05-23 09:09:08 +0300

    23-05- string compare

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -4,23 +4,13 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 0;
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
             char chLeft = left.charAt(i);
-            if (i > right.length() - 1) {
-                result = chLeft;
-                break;
-            } else if (chLeft == right.charAt(i)) {
-                continue;
-            } else {
-                result = Character.compare(chLeft, right.charAt(i));
-                break;
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
-        if (result == 0 && right.length() > left.length()) {
-            result = Integer.compare(left.length(), right.length());
-        }
-        return result;
+        return Integer.compare(left.length(), right.length());
     }
 
 }
