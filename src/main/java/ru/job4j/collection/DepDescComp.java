ea51edc13332bdbe5c0db99858a21f10ef55b5d1
Dimarik <krasnodar93@mail.ru>
2022-06-02 21:47:26 +0300

    02-06- сортировка2

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,14 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] s1 = o1.split("/");
         String[] s2 = o2.split("/");
         int rsl = s2[0].compareTo(s1[0]);
-        if (rsl != 0) {
-            return rsl != 0 ? rsl : o1.compareTo(o2);
-        }
-        for (int i = 1; i < Math.min(s1.length, s2.length); i++) {
-            if (!s1[i].equals(s2[i])) {
-                return s1[i].compareTo(s2[i]);
-            }
-        }
-        return Integer.compare(s1.length, s2.length);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
