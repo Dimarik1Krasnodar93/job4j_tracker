e6e513f1476b1eb0327de35fa56522a7a66fadad
Dimarik <krasnodar93@mail.ru>
2022-06-02 09:22:24 +0300

    02-06- 1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,59 @@
+package ru.job4j.lambda;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Attachment {
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                + "name='" + name + '\''
+                + ", size=" + size
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator<Attachment> comparator =  new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return Integer.compare(left.getSize(), right.getSize());
+            }
+        };
+        attachments.sort(comparator);
+        System.out.println(attachments);
+        List<Attachment> attachments2 = Arrays.asList(
+                new Attachment("_10image 1", 100),
+                new Attachment("_1image 2", 34),
+                new Attachment("_2image 3", 13)
+        );
+        Comparator<Attachment> comparator2 =  new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment left, Attachment right) {
+                return left.getName().compareTo(right.getName());
+            }
+        };
+        attachments2.sort(comparator2);
+        System.out.println(attachments2);
+    }
+}
