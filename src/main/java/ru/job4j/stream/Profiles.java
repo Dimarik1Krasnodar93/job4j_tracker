eb14227c02a0ef514a22dfbe15034122ea699fe9
Dimarik <krasnodar93@mail.ru>
2022-06-06 10:22:07 +0300

    06-06

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(x -> x.getAddress()).collect(Collectors.toList());
+        List<Address> addressList = new ArrayList<>();
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
     }
 }
