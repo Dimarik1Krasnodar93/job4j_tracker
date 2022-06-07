baffe1f0102b3af64948fe4251336c608c250c8e
Dimarik <krasnodar93@mail.ru>
2022-06-07 09:09:36 +0300

    07-06 5. Преобразование матрицы чисел в список чисел. [#110228]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -16,6 +16,9 @@ public class Profiles {
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream().map(x -> x.getAddress()).sorted(Comparator.comparing(e -> e.getCity())).distinct().collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
