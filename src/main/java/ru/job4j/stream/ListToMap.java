3075b868def53ef9e59bb71021a2474e801aa2dd
Dimarik <krasnodar93@mail.ru>
2022-06-07 09:25:48 +0300

    07-06 2

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -9,7 +9,7 @@ public class ListToMap {
 
     public static Map<String, Student> convert(List<Student> list) {
         return list.stream()
-                .collect(Collectors.toMap(e -> e.getSurname(),
+                .collect(Collectors.toMap(Student::getSurname,
                         e -> e,
                         (existing, replacement) -> existing));
     }
