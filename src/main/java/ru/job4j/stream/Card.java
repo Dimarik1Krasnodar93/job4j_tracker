feed250f2ebb626f828ebd040c25c01c74781ee5
Dimarik <krasnodar93@mail.ru>
2022-06-09 09:58:43 +0300

    09-06-0957

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+public class Card {
+    public enum Value {
+        V_6, V_7, V_8
+    }
+
+    public enum Suit {
+        Diamonds, Hearts, Spades, Clubs
+    }
+
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
---
+++ src/main/java/ru/job4j/stream/DoubleLoop.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class DoubleLoop {
+
+    public static void main(String[] args) {
+        String[] levels = {"level 1", "level 2", "level 3"};
+        String[] tasks = {"task 1", "task 2", "task 3", "task 4"};
+        Stream.of(levels)
+                .flatMap(level -> Stream.of(tasks)
+                        .map(task -> level + " " + task))
+                .forEach(System.out::println);
+        Card[] cardDesk = Stream.of(Card.Suit.values())
+                .flatMap(suit -> Stream.of(Card.Value.values()).map(value -> new Card(suit, value)))
+                .toArray(Card[]::new);
+    }
+}
